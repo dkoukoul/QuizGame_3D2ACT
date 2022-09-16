f8b792792b97e76a58d903ce81ddd3271ffceff7
dkoukoul <dimitris.koukoulakis@gmail.com>
2022-09-16 22:47:45 +0300

    add remaining globals

--- app/src/main/java/co/d2act/quizgame/Globals.kt
+++ app/src/main/java/co/d2act/quizgame/Globals.kt
@@ -7,30 +7,41 @@ object Globals {
     const val CLICK = 1
     const val SHAKE = 2
     const val SPEAK = 3
-    const val SCAN = 4
+    const val QRSCAN = 4
     const val COLOR = 5
     private var currentSection = 0
     private var currentQuestion = 0
 
     //private val types1 = arrayListOf(CLICK,SHAKE,SCAN)
-    private val types1 = arrayListOf(COLOR,SHAKE,SCAN)
-    private val types2 = arrayListOf(SCAN,SCAN,CLICK)
+    private val types1 = arrayListOf(CLICK,SHAKE,QRSCAN)
+    private val types2 = arrayListOf(QRSCAN,QRSCAN,CLICK)
     private val types3 = arrayListOf(SPEAK,SHAKE,CLICK)
-    private val types4 = arrayListOf(SCAN,SPEAK,SCAN)
+    private val types4 = arrayListOf(QRSCAN,SPEAK,QRSCAN)
     private val types5 = arrayListOf(CLICK,COLOR,CLICK)
+    private val types6 = arrayListOf(QRSCAN,SPEAK,QRSCAN)
+    private val types7 = arrayListOf(SPEAK,COLOR,SHAKE)
+    private val types8 = arrayListOf(QRSCAN,CLICK,COLOR)
+    private val types9 = arrayListOf(SPEAK,COLOR,CLICK)
+    private val types10 = arrayListOf(SHAKE,CLICK,QRSCAN)
+
     //ALL CLICK for DEBUGGING
 /*    val types1 = arrayListOf(CLICK,CLICK,CLICK)
     val types2 = arrayListOf(CLICK,CLICK,CLICK)
     val types3 = arrayListOf(CLICK,CLICK,CLICK)
     val types4 = arrayListOf(CLICK,CLICK,CLICK)
     val types5 = arrayListOf(CLICK,CLICK,CLICK)*/
-    val questionTypes = arrayListOf(types1,types2,types3,types4,types5)
+    val questionTypes = arrayListOf(types1,types2,types3,types4,types5,types6,types7,types8,types9,types10)
     private val answers1 = arrayListOf(3,3,3)
     private val answers2 = arrayListOf(2,2,2)
     private val answers3 = arrayListOf(1,2,3)
     private val answers4 = arrayListOf(1,3,2)
     private val answers5 = arrayListOf(2,1,1)
-    val answers = arrayListOf(answers1, answers2, answers3, answers4, answers5)
+    private val answers6 = arrayListOf(1,1,2)
+    private val answers7 = arrayListOf(2,3,2)
+    private val answers8 = arrayListOf(1,3,1)
+    private val answers9 = arrayListOf(1,2,1)
+    private val answers10 = arrayListOf(1,1,1)
+    val answers = arrayListOf(answers1, answers2, answers3, answers4, answers5, answers6, answers7, answers8, answers9, answers10)
     private var answeredQuestions : ArrayList<Int> = arrayListOf()
     private var score = 0
 
